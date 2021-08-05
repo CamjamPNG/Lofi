63303b01d9096369c2f0039992e01e1db53eb33b
camjam948 <38945005+camjam948@users.noreply.github.com>
2021-08-04 23:58:28 -0400

    Update settings.jsx

--- settings.jsx
+++ settings.jsx
@@ -7,12 +7,12 @@ module.exports = class LofiSettings extends React.PureComponent {
 		return (
 			<SliderInput
 				minValue={0}
-				maxValue={100}
-				initialValue={getSetting('volume', 1) * 100}
-				markers={[0, 25, 50, 75, 100]}
+				maxValue={200}
+				initialValue={getSetting('volume', 1) * 200)
+				markers={[0, 25, 50, 75, 100, 125, 50, 175, 200)
 				onValueChange={change => {
-					updateSetting('volume', change / 100);
-					window.sus.volume = change / 100;
+					updateSetting('volume', change / 200);
+					window.sus.volume = change / 200;
 				}}
 			>
 				Volume
