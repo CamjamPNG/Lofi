78eba71e5f1b7fbdb6ca624173d6cd80f0ed9aba
camjam948 <38945005+camjam948@users.noreply.github.com>
2021-08-05 00:18:52 -0400

    Update settings.jsx

--- settings.jsx
+++ settings.jsx
@@ -1,15 +1,9 @@
-const { React } = require('powercord/webpack');
-const { SliderInput } = require('powercord/components/settings');
-
-module.exports = class LofiSettings extends React.PureComponent {
-	render() {
-		const { getSetting, updateSetting } = this.props;
 		return (
 			<SliderInput
 				minValue={0}
-				maxValue={200}
-				initialValue={getSetting('volume', 1) * 200)
-				markers={[0, 25, 50, 75, 100, 125, 50, 175, 200)
+				maxValue={100}
+				initialValue={getSetting('volume', 1) * 200}
+				markers={[0, 25, 50, 75, 100, 125, 150, 175, 200]}
 				onValueChange={change => {
 					updateSetting('volume', change / 200);
 					window.sus.volume = change / 200;
