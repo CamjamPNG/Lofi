82dd4c1ad16cfe4717b1081d58c9d3619ea54e88
camjam948 <38945005+camjam948@users.noreply.github.com>
2021-08-04 23:00:36 -0400

    Add files via upload

---
+++ settings.jsx
@@ -0,0 +1,22 @@
+const { React } = require('powercord/webpack');
+const { SliderInput } = require('powercord/components/settings');
+
+module.exports = class LofiSettings extends React.PureComponent {
+	render() {
+		const { getSetting, updateSetting } = this.props;
+		return (
+			<SliderInput
+				minValue={0}
+				maxValue={100}
+				initialValue={getSetting('volume', 1) * 100}
+				markers={[0, 50, 100]}
+				onValueChange={change => {
+					updateSetting('volume', change / 100);
+					window.sus.volume = change / 100;
+				}}
+			>
+				Volume
+			</SliderInput>
+		);
+	}
+};
